06f48614675a76ae9dc8f8654e3da3c12d6c310f
DaniilVC <123402598+DaniilVC@users.noreply.github.com>
2025-03-12 21:11:42 +0300

    Update test.py

--- test.py
+++ test.py
@@ -37,8 +37,10 @@ dp.include_router(router)
 def create_main_menu():
     builder = InlineKeyboardBuilder()
     builder.add(
-        InlineKeyboardButton(text='Профиль', callback_data='profile'),
-        InlineKeyboardButton(text='Расписание', callback_data='schedule')
+        InlineKeyboardButton(text='Профиль', 
+                             callback_data='profile'),
+        InlineKeyboardButton(text='Расписание', 
+                             callback_data='schedule')
     )
     builder.adjust(1)
     return builder.as_markup()
@@ -47,17 +49,21 @@ def create_schedule_menu(user_id):
     builder = InlineKeyboardBuilder()
     
     builder.add(
-        InlineKeyboardButton(text='Просмотреть расписание', callback_data='view_schedule')
+        InlineKeyboardButton(text='Просмотреть расписание', 
+                             callback_data='view_schedule')
         )
 
     if has_elder_rights(user_id):
         builder.add(
-            InlineKeyboardButton(text='Добавить предмет', callback_data='add_subject'),
-            InlineKeyboardButton(text='Удалить предмет', callback_data='remove_subject'),
+            InlineKeyboardButton(text='Добавить предмет', 
+                                 callback_data='add_subject'),
+            InlineKeyboardButton(text='Удалить предмет', 
+                                 callback_data='remove_subject'),
         )
         
     builder.row(
-        InlineKeyboardButton(text='Назад', callback_data='back_to_main')
+        InlineKeyboardButton(text='Назад', 
+                             callback_data='back_to_main')
         )
 
     builder.adjust(1)
@@ -67,8 +73,10 @@ def create_profile_menu(user_id):
     builder = InlineKeyboardBuilder()
 
     builder.add(
-        InlineKeyboardButton(text="Изменить время напоминания", callback_data="edit_remind_time"),
-        InlineKeyboardButton(text='Назад', callback_data='back_to_main')
+        InlineKeyboardButton(text="Изменить время напоминания", 
+                             callback_data="edit_remind_time"),
+        InlineKeyboardButton(text='Назад', 
+                             callback_data='back_to_main')
     )
 
     builder.adjust(1)
